8eb8bd9aa6220271920bb79d72540e49499099b8
myungkang2020 <kmkaero3746@gmail.com>
2021-08-27 23:35:48 +0900

    [Check Option] Input compare & print

---
+++ BatchArg2Xml/BatchArg2Xml/BatchArgInputPrint/BatchArgInputPrint.cpp
@@ -0,0 +1,42 @@
+// BatchArgInputPrint.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <vector>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+	string arg_batch;
+	vector<string> arg;
+
+	// check option
+	try
+	{
+		if (strcmp(argv[1], "batch")) {
+			throw runtime_error("Please Enter Correct Options");
+		}
+		else { /* do nothing */ }
+	}
+	catch (const std::exception& e) { cout << e.what() << endl; }
+
+	for (int i = 0; i < argc; ++i)
+	{
+		cout << "argv[" << i << "]   :" << argv[i] << "\n";
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
